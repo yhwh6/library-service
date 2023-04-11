0c06134dff460f0e790d383b6a0ff09a800ea1ed
Maksym_Sylantiev <maxym2s@yahoo.com>
2023-04-11 15:42:03 +0300

    Created models: users, books, borrowings & updated settings

--- books/models.py
+++ books/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    class BookCover(models.TextChoices):
+        HARD = "H"
+        SOFT = "S"
+
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    cover = models.CharField(max_length=1, choices=BookCover.choices)
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField(max_digits=7, decimal_places=2)
+
+    def get_cover(self):
+        cover_index = self.BookCover.values.index(self.cover)
+        return self.BookCover.labels[cover_index]
